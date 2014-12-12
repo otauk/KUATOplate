1823d64197c7b521a2b29a27797a172bde853b51
KUATOproject <KUATOimac@admin.lehrer.ruhr-uni-bochum.de>
2014-12-12 14:16:41 +0100

    startup

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>westenholz600</title>
+		<!-- Font -->
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+		<!-- Stylesheet -->
+		<link href="css/custom-base.css" rel="stylesheet" type="text/css"/>
+		<!-- jQuery -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="header">
+				Header
+			</div>
+			<div class="navi">
+				Navigation
+			</div>
+			<div class="main">
+				<h1>Headline h1</h1>
+				<p>
+					Some text in a paragraph
+				</p>
+				<h2>headline h2</h2>
+				<p>
+					Lorem ipsum dolor sit amet, <a href="#">consectetur adipisicing elit</a>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<button class="btn bgcolor-1">Button 1</button>
+				<button class="btn bgcolor-2">Button 2</button>
+				<button class="btn bgcolor-3">Button 3</button>
+				<button class="btn bgcolor-4">Button 4</button>
+				<button class="btn bgcolor-5">Button 5</button>
+				<div class="alert positive">Bestätigung</div>
+				<div class="alert negative">Warnung</div>
+				<div class="alert neutral">Hinweis</div>
+
+
+				<ul class="navlist">
+					<a href="#">
+						<li>Item 1</li>
+					</a>
+					<a href="#">
+					<li>Item 2
+						<ul class="subnavlist">
+							<a href="#">
+								<li>Sub-Item 2.1</li>
+							</a>
+							<a href="#">
+								<li>Sub-Item 2.2</li>
+							</a>
+						</ul>
+					</li>
+					</a>
+					<a href="#">
+						<li>Item 3</li>
+					</a>
+				</ul>
+				<div class="content">
+					Content
+				</div>
+			</div>
+			<div class="footer dev_border">Footer</div>
+		</div>
+	</body>
+</script>
+</html>
