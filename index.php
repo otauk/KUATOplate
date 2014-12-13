707cab8fd921edb8442fe53e628c620ab36bbcb7
KUATOproject <kuato@gmx.net>
2014-12-13 04:06:01 +0100

    less Datei ausgebaut, 1. Entwurf steht

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>westenholz600</title>
+		<title>KUATOplate</title>
 		<!-- Font -->
-		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<!-- ~~~~~~~~~~~~~~~~~~~~| FONT |~~~~~~~~~~~~~~~~~~~~ -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
 		<!-- Stylesheet -->
 		<link href="css/custom-base.css" rel="stylesheet" type="text/css"/>
 		<!-- jQuery -->
@@ -11,11 +13,13 @@
 	</head>
 	<body>
 		<div class="wrapper">
-			<div class="header">
-				Header
-			</div>
+			<div class="header"></div>
 			<div class="navi">
-				Navigation
+				<ul>
+					<li class="active">Page 1</li>
+					<li>Page 2</li>
+					<li>Page 3</li>
+				</ul>
 			</div>
 			<div class="main">
 				<h1>Headline h1</h1>
@@ -26,6 +30,50 @@
 				<p>
 					Lorem ipsum dolor sit amet, <a href="#">consectetur adipisicing elit</a>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
+				<p>
+					<input type="text" name="name" />
+				</p>
+				<ul>
+					<li>Item 1</li>
+					<li>Item 2
+						<ul>
+							<li>Sub-Item 2.1</li>
+							<li>Sub-Item 2.2
+								<ul>
+										<li>Sub-Item 2.2.1</li>
+										<li>Sub-Item 2.2.2</li>
+								</ul>
+							</li>
+							<li>Sub-Item 2.3</li>
+						</ul>
+					</li>
+					<li>Item 3</li>
+				</ul>
+
+				<table class="tbl">
+					<tr>
+						<th>Column 1</th>
+						<th>Column 2</th>
+						<th>Column 3</th>
+					</tr>
+					<tr>
+						<td>Block 1</td>
+						<td>Block 2</td>
+						<td>Block 3</td>
+					</tr>
+					<tr>
+						<td>Block 1</td>
+						<td>Block 2</td>
+						<td>Block 3</td>
+					</tr>
+					<tr>
+						<td>Block 1</td>
+						<td>Block 2</td>
+						<td>Block 3</td>
+					</tr>
+				</table>
+
+
 				<button class="btn bgcolor-1">Button 1</button>
 				<button class="btn bgcolor-2">Button 2</button>
 				<button class="btn bgcolor-3">Button 3</button>
@@ -34,33 +82,9 @@
 				<div class="alert positive">Bestätigung</div>
 				<div class="alert negative">Warnung</div>
 				<div class="alert neutral">Hinweis</div>
-
-
-				<ul class="navlist">
-					<a href="#">
-						<li>Item 1</li>
-					</a>
-					<a href="#">
-					<li>Item 2
-						<ul class="subnavlist">
-							<a href="#">
-								<li>Sub-Item 2.1</li>
-							</a>
-							<a href="#">
-								<li>Sub-Item 2.2</li>
-							</a>
-						</ul>
-					</li>
-					</a>
-					<a href="#">
-						<li>Item 3</li>
-					</a>
-				</ul>
-				<div class="content">
-					Content
-				</div>
 			</div>
-			<div class="footer dev_border">Footer</div>
+			<div class="footer">
+			</div>
 		</div>
 	</body>
 </script>
